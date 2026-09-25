67beedb38578b7d191f91f9a0e047057493b634c
Potteh <58096074+Potteh@users.noreply.github.com>
2026-09-25 15:06:11 -0400

    dfesdwfrt

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,22 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-ui_page 'index.html'
+this_is_a_map "yes"
+
+data_file 'TIMECYCLEMOD_FILE' 'brnx_lsmedical_timecycle.xml'
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'alta.ymt'
+data_file 'SCENARIO_INFO_FILE' 'alta.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'alta.ymt'
+data_file 'AUDIO_GAMEDATA' 'audio/brnx_vinemed_audios.dat'
+
+replace_level_meta 'gta5'
 
 files {
-    'index.html',
-    'style.css',
-    'script.js',
-}
-
-client_scripts {
-    'config.lua',
-    'client.lua'
+    'audio/brnx_vinemed_audios.dat151.rel',
+    'stream/vanilla/metadata/alta.ymt',
+    'gta5.meta',
+    'doortuning.ymt',
+    'brnx_lsmedical_timecycle.xml'
 }
 
